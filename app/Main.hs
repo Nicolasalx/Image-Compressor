3b061def187d05358bd1b2aecd00703f061521fc
thibaudcathala <thibaud.cathala@epitech.eu>
2024-03-04 10:28:53 +0100

    add: basic main that return 0 and all the stack and makefile

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+--
+-- EPITECH PROJECT, 2024
+-- B-FUN-400-PAR-4-1-compressor-thibaud.cathala
+-- File description:
+-- Main
+--
+
+module Main (main) where
+
+import Lib
+
+main :: IO ()
+main = someFunc
