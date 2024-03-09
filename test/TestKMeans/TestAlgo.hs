7342b0377a9fa9db4669d9a4404cdbd6091ec97b
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-03-09 22:40:06 +0100

    mod: units tests modified with the new infrastrucutre of the algorithm

--- test/TestKMeans/TestAlgo.hs
+++ test/TestKMeans/TestAlgo.hs
@@ -7,25 +7,19 @@
 
 module TestKMeans.TestAlgo (testAlgoKMeans) where
 
-import KMeans.Compression (euclidianDistance,
-    euclidianDistanceCent,
-    initCentroid,
-    cmpCentroidColor,
-    findClosestCentroid,
-    appendColorToCentroid,
-    assignDataPoint,
-    initCentroidList)
-import DataStruct (Pixel(..), Centroid(..))
+import KMeans.EuclidianDistance (euclidianDistance, euclidianDistanceCent)
+import KMeans.GetFirstNColor (initCentroid)
+import DataStruct (Centroid(..), Color(..))
 import Test.HUnit
 
 testEuclidianDistance :: Test
 testEuclidianDistance = TestList
     [ "Test case 1" ~:
-        euclidianDistance (Centroid 1.0 1.0 1.0) (Pixel 0 0 0 0 0) ~?= sqrt 3
+        euclidianDistance (Centroid 1.0 1.0 1.0) (Color 0 0 0) ~?= sqrt 3
     , "Test case 2" ~:
-        euclidianDistance (Centroid 0.0 0.0 0.0) (Pixel 1 1 1 1 1) ~?= sqrt 3
+        euclidianDistance (Centroid 0.0 0.0 0.0) (Color 1 1 1) ~?= sqrt 3
     , "Test case 3" ~:
-        euclidianDistance (Centroid 0.0 0.0 0.0) (Pixel 0 0 0 0 0) ~?= 0.0
+        euclidianDistance (Centroid 0.0 0.0 0.0) (Color 0 0 0) ~?= 0.0
     ]
 
 testEuclidianDistanceCent :: Test
@@ -51,71 +45,10 @@ testInitCentroid = TestList
         initCentroid [] ~?= ([] :: [Centroid])
     ]
 
-testCmpCentroidColor :: Test
-testCmpCentroidColor = TestList
-    [ "Test case 1" ~:
-        cmpCentroidColor (Centroid 1.0 1.0 1.0) (Pixel 0 0 0 0 0)
-            ((Centroid 2.0 2.0 2.0), 1.0) ~?= ((Centroid 2.0 2.0 2.0), 1.0)
-    , "Test case 2" ~:
-        cmpCentroidColor (Centroid 0.0 0.0 0.0) (Pixel 1 1 1 1 1)
-            ((Centroid 0.0 0.0 0.0), 0.0) ~?= ((Centroid 0.0 0.0 0.0), 0.0)
-    , "Test case 3" ~:
-        cmpCentroidColor (Centroid 2.0 2.0 2.0) (Pixel 0 0 0 0 0)
-            ((Centroid 1.0 1.0 1.0), 0.5) ~?= ((Centroid 1.0 1.0 1.0), 0.5)
-    ]
-
-testFindClosestCentroid :: Test
-testFindClosestCentroid = TestList
-    [ "Test case 1" ~:
-        findClosestCentroid [Centroid 1.0 1.0 1.0, Centroid 2.0 2.0 2.0]
-            (Pixel 0 0 0 0 0) ((Centroid 3.0 3.0 3.0), 1.0) ~?=
-            ((Centroid 3.0 3.0 3.0), 1.0)
-    , "Test case 2" ~:
-        findClosestCentroid [Centroid 0.0 0.0 0.0] (Pixel 1 1 1 1 1)
-            ((Centroid 0.0 0.0 0.0), 0.0) ~?= ((Centroid 0.0 0.0 0.0), 0.0)
-    , "Test case 3" ~:
-        findClosestCentroid [] (Pixel 0 0 0 0 0) ((Centroid 1.0 1.0 1.0), 0.5) ~?=
-            ((Centroid 1.0 1.0 1.0), 0.5)
-    ]
-
-testInitCentroidList :: Test
-testInitCentroidList = TestList
-    [ "Test case 1" ~:
-        initCentroidList [Centroid 1.0 2.0 3.0, Centroid 4.0 5.0 6.0] ~?= [(Centroid 1.0 2.0 3.0, []), (Centroid 4.0 5.0 6.0, [])]
-    , "Test case 2" ~:
-        initCentroidList [Centroid 0.0 0.0 0.0] ~?= [(Centroid 0.0 0.0 0.0, [])]
-    ]
-
-testAppendColorToCentroid :: Test
-testAppendColorToCentroid = TestList
-    [ "Test case 1" ~:
-        appendColorToCentroid (Centroid 1.0 2.0 3.0) (Pixel 0 0 0 0 0)
-            [(Centroid 1.0 2.0 3.0, [])] ~?= [(Centroid 1.0 2.0 3.0,
-            [Pixel 0 0 0 0 0])]
-    , "Test case 2" ~:
-        appendColorToCentroid (Centroid 0.0 0.0 0.0) (Pixel 1 1 1 1 1)
-            [(Centroid 0.0 0.0 0.0, [])] ~?= [(Centroid 0.0 0.0 0.0,
-            [Pixel 1 1 1 1 1])]
-    ]
-
-testAssignDataPoint :: Test
-testAssignDataPoint = TestList
-    [ "Test case 1" ~:
-        assignDataPoint [Centroid 1.0 2.0 3.0, Centroid 4.0 5.0 6.0]
-            [Pixel 0 0 0 0 0, Pixel 1 1 1 1 1] [] ~?= []
-    , "Test case 2" ~:
-        assignDataPoint [Centroid 0.0 0.0 0.0]
-            [Pixel 1 1 1 1 1, Pixel 2 2 2 2 2] [] ~?= []
-    ]
 
 testAlgoKMeans :: Test
 testAlgoKMeans = test [
         testEuclidianDistance,
         testEuclidianDistanceCent,
-        testInitCentroid,
-        testCmpCentroidColor,
-        testFindClosestCentroid,
-        testAppendColorToCentroid,
-        testAssignDataPoint,
-        testInitCentroidList
+        testInitCentroid
     ]
